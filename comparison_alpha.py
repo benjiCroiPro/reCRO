0d32cc5711819ea38fd45be9124c8c7dfa473fde
Benji Cohen <benji.cohen@iprospect.com>
2018-03-18 21:37:31 +0000

    Updated virtual product structure and implemented a product relevance function, broke user journey comparison

--- comparison_alpha.py
+++ comparison_alpha.py
@@ -16,6 +16,7 @@ if products_json.is_file() and user_profile_json.is_file():
 		user_profile = json.load(user)
 		user_id = user_profile['user_id']
 		user_journey = user_profile["user_journey"]
+		user_page = user_profile["current_page"]
 		# open product database
 		with open("databases/products.json", 'r') as prods:
 			# relevant messaging
@@ -23,7 +24,19 @@ if products_json.is_file() and user_profile_json.is_file():
 			products = p_json["products"]
 			# find journey similarity
 			for product in products:
+				if product["product_url"] == user_page:
+					current_product = product
+					products.remove(product)
+			
+			for product in products:
 				journey_similarity = recro_functions.journey_comparison(product, user_journey)
-				product["journey_similarity"] = journey_similarity
+				for pj in product["user_journeys"]:
+					print (journey_similarity)
+					for js in journey_similarity:
+						pj["journey_similarity"] = js["similarity"]
+						pj["journey_conversion_rate"] = js["conversion_rate"]
+
+				product_relevance = recro_functions.product_relevance(product, current_product)
+				product["product_relevance"] = product_relevance
 
 			print(json.dumps(products, indent=4))
--- recro_functions.py
+++ recro_functions.py
@@ -8,8 +8,9 @@ def journey_comparison(product, user_journey):
 		similarity_points = 0
 		pages = journey["pages"]
 		for page in pages:
-			if pages[page] == user_journey[str(x)]:
-				similarity_points+=1
+			for uj in user_journey:
+				if pages[page] == uj:
+					similarity_points+=1
 			x+=1;
 		similarity = similarity_points/x
 		conversion_rate = int(journey["journey_conversions"])/int(journey["journey_count"])
@@ -18,3 +19,27 @@ def journey_comparison(product, user_journey):
 
 	similar_journeys = sorted(similar_journeys, key=lambda x: (x["path_id"], x["conversion_rate"]), reverse=True)
 	return similar_journeys[:3]
+
+def product_relevance(productA, productB):
+	productA_categories = productA["product_categories"]
+	productA_related_products = productA["related_products"]
+
+	productB_categories = productB["product_categories"]
+	productB_related_products = productB["related_products"]
+
+	primary_match = 0
+	secondary_match = 0
+	
+	if productA_categories["primary"] == productB_categories["primary"]:
+		primary_match += 1
+	elif productA_categories["primary"] == productB_categories["secondary"]:
+		primary_match += 1
+	
+	if productA_categories["secondary"] == productB_categories["primary"]:
+		secondary_match += 1
+	elif productA_categories["secondary"] == productB_categories["secondary"]:
+		secondary_match += 1
+
+	relevance = primary_match + (secondary_match / 2)
+
+	return relevance
